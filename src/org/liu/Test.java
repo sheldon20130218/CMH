bc26cf34adc8a2c43dbbe0521006e053523e9757
Administrator <Administrator@192.168.42.2>
2021-06-19 10:33:28 +0800

    test project

---
+++ src/org/liu/Test.java
@@ -0,0 +1,16 @@
+package org.liu;
+
+import java.util.Date;
+
+/**
+ * @author Administrator
+ *
+ */
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Helloworld"+new Date());
+	}
+
+}
